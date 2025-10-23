7b226e74c4f7c1a9935269bee699eb9425806a6a
syunngiku-miniPC <syunngikusub3@outlook.jp>
2025-10-23 21:04:25 +0900

    .

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 class Roles():
     rsl1: int = 1
-    rsl2: int = 1392060380561408052
-    rsc1: int = 1397899288553717770
-    rsc2: int = 1
+    rsl2: int = 1232143915449188482
+    rsc1: int = 1232143942590533643
+    rsc2: int = 1232143965634170890
     rsc3: int = 1
     rsc4: int = 1
     rsc5: int = 1
@@ -11,27 +11,19 @@ class Roles():
     rsc8: int = 1
     rsc9: int = 1
     rsc10: int = 1
-    administrater_role_id: int = 1  # 鯖管理者ロールID
+    administrater_role_id: int = 1215981050292080640  # 鯖管理者ロールID
 
 
 class Channels():
-    important_log: int = 1399287484621262979  # 重要ログチャンネル#//Now：テスト鯖の重要logチャンネル
-    log: int = 1399287447753588848  # ログチャンネル#//Now：テスト鯖のlogチャンネル
+    important_log: int = 1
 
 
 class Categories():
-    announcement: int = 1  # 「重要項目」カテゴリー
-    poll: int = 1  # 「投票用カテゴリー」
-    server_management: int = 1  # 「鯖運営について」カテゴリー
-    freechat: int = 1  # 「お話しするチャット」カテゴリー
-    developer: int = 1397904744227278918  # 「開発カテゴリー」#//Now：テスト鯖の春菊の調理場
-    information: int = 1  # 「情報チャット」カテゴリー
-    showcase: int = 1  # 「掲示するところ」カテゴリー
-    public_meeting: int = 1  # 「公開会議」カテゴリー
+    announcement: int = 1  
 
 
 def load_token() -> str:
-    with open("SybotTK.txt", "r") as f:
+    with open("TSTK.txt", "r") as f:
         d = f.read()
     return d
 
