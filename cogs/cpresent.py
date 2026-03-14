5cf73428e2061d5f73e7b1cad881ef07f2f81e67
Syunngiku0402 <haruokcxj01524@docomo.ne.jp>
2026-03-14 23:47:24 +0900

    ついか

--- cogs/cpresent.py
+++ cogs/cpresent.py
@@ -80,6 +80,7 @@ class Present(commands.Cog):
             color=0x87ea00,
             timestamp=datetime.now()
         )
+        present_embed.set_footer(text="Bot開発・主催:菊菜(きくな)")
         await interaction.response.send_message("送信しました", ephemeral=True)
         await interaction.channel.send(embed=present_embed)
         await interaction.channel.send(view=Lottery(self.bot))
