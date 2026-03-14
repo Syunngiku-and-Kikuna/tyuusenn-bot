d0449461a1b4c7f358cdc618c692e214fbc033e8
Syunngiku0402 <haruokcxj01524@docomo.ne.jp>
2026-03-14 23:39:29 +0900

    新たな抽選

--- cogs/cpresent.py
+++ cogs/cpresent.py
@@ -1,10 +1,10 @@
-from discord.ext import commands
+from discord.ext import commands, tasks
 from datetime import datetime, timedelta
 from discord import ButtonStyle, app_commands
 from database import User, session
 import discord
 from config import config
-import asyncio
+import random
 
 
 class Lottery(discord.ui.View):  # 抽選コマンド
@@ -12,152 +12,70 @@ class Lottery(discord.ui.View):  # 抽選コマンド
         super().__init__(timeout=None)
         self.bot = bot
 
-    @discord.ui.button(label="応募", style=ButtonStyle.green, emoji="✅", custom_id="present")
+    @discord.ui.button(label="参加", style=ButtonStyle.green, emoji="✅", custom_id="present")
     async def pressedLotteryButton(self, interaction: discord.Interaction, button: discord.ui.button):
         await interaction.response.defer(thinking=True, ephemeral=True)
         oubouser = session.query(User).filter_by(userid=interaction.user.id).first()
-        rsl2 = interaction.guild.get_role(config.role.rsl2)
-        rsc1 = interaction.guild.get_role(config.role.rsc1)
-        rsc2 = interaction.guild.get_role(config.role.rsc2)
-        rsc3 = interaction.guild.get_role(config.role.rsc3)
-        rsc4 = interaction.guild.get_role(config.role.rsc4)
-        rsc5 = interaction.guild.get_role(config.role.rsc5)
-        point = 0
+
         if oubouser is None:
-            userdb = User(userid=interaction.user.id, username=interaction.user.name)
+            userdb = User(userid=interaction.user.id, username=interaction.user.name, displayname=interaction.user.display_name)
             session.add(userdb)
             session.commit()
-            await interaction.followup.send("データベースが作成されました。もう一度[応募ボタン]を押してください\n-# 1回で応募まで完了させたかったんだけど多分仕様的に無理っぽい(笑)", ephemeral=True)
-            return
-        if oubouser.oubo is True:
-            await interaction.followup.send("すでに応募済みです。抽選開始までお待ちください。", ephemeral=True)
-            oubouser.pushbutton += 1
-            session.commit()
-            return
-        if rsl2 in interaction.user.roles:
-            oubouser.rsl2 = True
-            point += 1
-            await asyncio.sleep(1)  # 少し待機してから再取得
-        if rsc1 in interaction.user.roles:
-            oubouser.rsc1 = True
-            point += 1
-            await asyncio.sleep(1)  # 少し待機してから再取得
-        if rsc2 in interaction.user.roles:
-            oubouser.rsc2 = True
-            point += 1
-            await asyncio.sleep(1)  # 少し待機してから再取得
-        if rsc3 in interaction.user.roles:
-            oubouser.rsc3 = True
-            point += 1
-            await asyncio.sleep(1)  # 少し待機してから再取得
-        if rsc4 in interaction.user.roles:
-            oubouser.rsc4 = True
-            point += 1
-            await asyncio.sleep(1)  # 少し待機してから再取得
-        if rsc5 in interaction.user.roles:
-            oubouser.rsc5 = True
-            point += 1
-            await asyncio.sleep(1)  # 少し待機してから再取得
-        if point == 0:
-            await interaction.followup.send("応募資格がありません\n応募条件を読んできてください", ephemeral=True)
-            oubouser.pushbutton += 1
-            session.commit()
+            await interaction.followup.send("参加が完了しました。当選発表までお待ちください。", ephemeral=True)
             return
         else:
-            oubouser.oubo = True  # 応募済み
-            oubouser.pushbutton += 1
-            session.commit()
-            await interaction.followup.send(f"{point}口応募されました。抽選開始までお待ちください。", ephemeral=True)
+            await interaction.followup.send("すでに参加済みです。当選発表までお待ちください。", ephemeral=True)
+            return
 
-    @discord.ui.button(label="応募状況確認", style=ButtonStyle.blurple, custom_id="check")
+    @discord.ui.button(label="参加状況確認", style=ButtonStyle.blurple, custom_id="check")
     async def pressedCheckButton(self, interaction: discord.Interaction, button: discord.ui.button):
         oubouser = session.query(User).filter_by(userid=interaction.user.id).first()
         if oubouser is None:
-            await interaction.response.send_message("まだ応募されていません。応募ボタンを押して応募してください。", ephemeral=True)
+            await interaction.response.send_message("まだ参加されていません。参加ボタンを押して参加してください。", ephemeral=True)
             return
-        if oubouser.oubo is True:
-            rsl2status = "取得済み(応募権)" if oubouser.rsl2 else "なし"
-            rsc1status = "取得済み(応募権)" if oubouser.rsc1 else "なし"
-            rsc2status = "取得済み(応募権)" if oubouser.rsc2 else "なし"
-            rsc3status = "取得済み(応募権)" if oubouser.rsc3 else "なし"
-            rsc4status = "取得済み(応募権)" if oubouser.rsc4 else "なし"
-            rsc5status = "取得済み(応募権)" if oubouser.rsc5 else "なし"
-            CHDESC = f"""
-<@{interaction.user.id}> さんの応募状況は以下の通りです。
-応募権1つにつき1口応募されます。
-
-> <@&{config.role.rsl2}> : {rsl2status}
-> <@&{config.role.rsc1}> : {rsc1status}
-> <@&{config.role.rsc2}> : {rsc2status}
-> <@&{config.role.rsc3}> : {rsc3status}
-> <@&{config.role.rsc4}> : {rsc4status}
-> <@&{config.role.rsc5}> : {rsc5status}
-
-合計応募口数 : {sum([oubouser.rsl2, oubouser.rsc1, oubouser.rsc2, oubouser.rsc3, oubouser.rsc4, oubouser.rsc5])}口
-"""
-            chembed = discord.Embed(
-                title="応募状況確認",
-                description=CHDESC,
-                color=0x00ff00
-            )
-            await interaction.response.send_message("応募済みです。抽選開始までお待ちください。", embed=chembed, ephemeral=True)
         else:
-            await interaction.response.send_message("まだ応募されていません。応募ボタンを押して応募してください。", ephemeral=True)
+            await interaction.response.send_message("参加完了してます。当選発表までお待ちください。", ephemeral=True)
 
     @discord.ui.button(label="企画終了", style=ButtonStyle.red, custom_id="delevent")
     async def pressedDeleventButton(self, interaction: discord.Interaction, button: discord.ui.button):
-        role = interaction.guild.get_role(config.role.administrater_role_id)
-        if role in interaction.user.roles:
+        if interaction.user.id == config.syunngiku_id:
             await interaction.message.delete()
         else:
             await interaction.response.send_message("権限ないで", ephemeral=True)
-            oubouser = session.query(User).filter_by(userid=interaction.user.id).first()
-            oubouser.pushbutton += 1
-            session.commit()
 
 
 class Present(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+        self.send_task.start()
 
 # !--------------------------------------------------------------------
 
     @app_commands.command(name="present", description="【運営】present企画")
-    @app_commands.describe(
-        serveruser="サーバー人数(100の倍数で入力)",
-        kikann="応募期間(日数入力)"
-    )
     @app_commands.checks.has_role(config.role.administrater_role_id)
     @app_commands.guild_only()
-    async def cpresent_user(self, interaction: discord.Interaction, serveruser: int, kikann: int):
-
-        syuuryoubi = datetime.now() + timedelta(days=kikann)
-        fsyuuryoubi = syuuryoubi.strftime(" %Y/%m/%d ")
-        tyuusennbi = syuuryoubi + timedelta(days=1)
-        ftyuusennbi = tyuusennbi.strftime(" %Y/%m/%d ")
+    async def cpresent_user(self, interaction: discord.Interaction):
 
         PRESENT_DESCRIPTION = f"""
 【応募条件】
 1: このサーバーに抽選時に参加していること
-2: 過去にれぞらんど/緑黄色クラフトに参加していること
-(Resoland2参加者,Ryokuousyoku1,2,3,4,5参加者ロールのいずれかを持っていること)
-3: 下のボタンを押すこと
+2: 下のボタンを押すこと
+([Twitter](https://x.com/Kikuna0402)をフォローしてくれると主が喜びます)
 
 【景品内容】
-1名 : Discord Nitro 1ヶ月分
+1名 : 星街すいせいバンドル(3000V-BUCKSのやつ) or 1000V-BUCKS(ギフトカードのやつ)
 
 【その他】
 ・2アカウント以上の応募・ボタンの連打は禁止です
-→発覚次第その回の全アカウントでの応募権はく奪します
-・参加者ロールが多いほど当選確率が上がります
-(Ryokuousyoku1～5参加者ロール持ってたら5口応募になります)
-・鯖主は参加権ないです(笑)
+→発覚次第応募権はく奪します
+・当選確率アップはありません
+・景品はどちらか選べます
 
-【締め切り】{fsyuuryoubi} 23:59
-【当選発表】{ftyuusennbi} 18:00
+【締め切り】{datetime.now().strftime('%Y/%m/%d')} 23:59
+【当選発表】{(datetime.now() + timedelta(days=1)).strftime('%Y/%m/%d')} 00:00
 """
         present_embed = discord.Embed(
-            title=f"{serveruser}人突破記念プチプレゼント企画!",
+            title="すいちゃんスキンプレゼント企画!",
             description=PRESENT_DESCRIPTION,
             color=0x87ea00,
             timestamp=datetime.now()
@@ -166,16 +84,25 @@ class Present(commands.Cog):
         await interaction.channel.send(embed=present_embed)
         await interaction.channel.send(view=Lottery(self.bot))
 
-    @app_commands.command(name="present-reset", description="【運営】present企画-リセットコマンド")
-    @app_commands.checks.has_role(config.role.administrater_role_id)
-    @app_commands.guild_only()
-    async def cpresentreset(self, interaction: discord.Interaction):
-        results = session.query(User).all()
-        for i in results:
-            i.oubo = False  # 応募済みをリセット
-        session.commit()
-        print("リセット完了")
-        await interaction.response.send_message("リセットしました", ephemeral=True)
+    @tasks.loop(seconds=60)
+    async def send_task(self):
+        now = datetime.now()
+        if now.hour == 23 and now.minute == 38:
+            ch = await self.bot.fetch_channel(config.channel.nitice)
+            oubouser = session.query(User).all()
+            if len(oubouser) == 0:
+                await ch.send("応募者はいませんでした。")
+                return
+            winner_num = random.randint(1, len(oubouser))
+            for user in oubouser:
+                if user.no == winner_num:
+                    embed = discord.Embed(
+                        title="当選発表",
+                        description=f"# 当選者発表\n## <@{user.userid}>さん\nおめでとうございます!!!\n景品は星街すいせいバンドルか1000V-BUCKSのどちらかになります。\nご希望の景品をDMにてお知らせください。",
+                        color=0x87ea00,
+                        timestamp=datetime.now()
+                    )
+                    await ch.send(embed=embed)
 
 
 async def setup(bot: commands.Bot):
--- database.py
+++ database.py
@@ -1,9 +1,9 @@
 from discord.ext import commands
-from sqlalchemy import Column, Integer, String, Boolean, create_engine
+from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = 'sqlite:///cmdlab_users.db'  # データベースの種類と名前をここで指定できます
+DATABASE_URL = 'sqlite:///database/users.db'  # データベースの種類と名前をここで指定できます
 engine = create_engine(DATABASE_URL)  # データベースエンジンを作成
 Base = declarative_base()            # データベースの親クラスを作成
 
@@ -13,131 +13,12 @@ class User(Base):
     no = Column(Integer, primary_key=True)
     userid = Column(Integer)
     username = Column(String)
-    oubo = Column(Boolean, default=False)
-    rsl1 = Column(Boolean, default=False)
-    rsl2 = Column(Boolean, default=False)
-    rsc1 = Column(Boolean, default=False)
-    rsc2 = Column(Boolean, default=False)
-    rsc3 = Column(Boolean, default=False)
-    rsc4 = Column(Boolean, default=False)
-    rsc5 = Column(Boolean, default=False)
-    rsc6 = Column(Boolean, default=False)
-    rsc7 = Column(Boolean, default=False)
-    rsc8 = Column(Boolean, default=False)
-    rsc9 = Column(Boolean, default=False)
-    rsc10 = Column(Boolean, default=False)
-    pushbutton = Column(Integer, default=0)
-    str1 = Column(String, default="")
-    str2 = Column(String, default="")
-    str3 = Column(String, default="")
-    str4 = Column(String, default="")
-    str5 = Column(String, default="")
-    str6 = Column(String, default="")
-    str7 = Column(String, default="")
-    str8 = Column(String, default="")
-    str9 = Column(String, default="")
-    str10 = Column(String, default="")
-    int1 = Column(Integer, default=0)
-    int2 = Column(Integer, default=0)
-    int3 = Column(Integer, default=0)
-    int4 = Column(Integer, default=0)
-    int5 = Column(Integer, default=0)
-    int6 = Column(Integer, default=0)
-    int7 = Column(Integer, default=0)
-    int8 = Column(Integer, default=0)
-    int9 = Column(Integer, default=0)
-    int10 = Column(Integer, default=0)
-    bool1 = Column(Boolean, default=False)
-    bool2 = Column(Boolean, default=False)
-    bool3 = Column(Boolean, default=False)
-    bool4 = Column(Boolean, default=False)
-    bool5 = Column(Boolean, default=False)
-    bool6 = Column(Boolean, default=False)
-    bool7 = Column(Boolean, default=False)
-    bool8 = Column(Boolean, default=False)
-    bool9 = Column(Boolean, default=False)
-    bool10 = Column(Boolean, default=False)
+    displayname = Column(String)
 
 
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
-
-
-# DATABASE_URL2 = 'sqlite:///ore_gacha.db'  # データベースの種類と名前をここで指定できます
-# engine2 = create_engine(DATABASE_URL2)  # データベースエンジンを作成
-# Base2 = declarative_base()            # データベースの親クラスを作成
-# 
-# 
-# class Oregacha(Base2):
-#     __tablename__ = 'usersgacha'
-#     no = Column(Integer, primary_key=True)
-#     userid = Column(Integer)
-#     username = Column(String)
-#     allcount = Column(Integer, default=0)
-# 
-#     netheritei = Column(Integer, default=0)
-#     netherites = Column(Integer, default=0)
-#     lapis = Column(Integer, default=0)
-#     diamond = Column(Integer, default=0)
-#     gold = Column(Integer, default=0)
-#     redstone = Column(Integer, default=0)
-#     emerald = Column(Integer, default=0)
-#     iron = Column(Integer, default=0)
-#     copper = Column(Integer, default=0)
-#     quartz = Column(Integer, default=0)
-#     coal = Column(Integer, default=0)
-#     breaking_pickaxe = Column(Integer, default=0)
-#     broken_pickaxe = Column(Integer, default=0)
-#     death = Column(Integer, default=0)
-# 
-#     beacon = Column(Integer, default=0)
-#     netheriteb = Column(Integer, default=0)
-#     lapisb = Column(Integer, default=0)
-#     diamondb = Column(Integer, default=0)
-#     goldb = Column(Integer, default=0)
-#     redstoneb = Column(Integer, default=0)
-#     emeraldb = Column(Integer, default=0)
-#     ironb = Column(Integer, default=0)
-#     copperb = Column(Integer, default=0)
-#     quartzb = Column(Integer, default=0)
-#     coalb = Column(Integer, default=0)
-#     broken_pickaxe9 = Column(Integer, default=0)
-#     death9 = Column(Integer, default=0)
-#     unkownworld = Column(Integer, default=0)
-# 
-#     dailygacha = Column(Integer, default=0)
-# 
-#     ogstr1 = Column(String, default="")  # ogstr1 : cog.core_gacha.py使用中(１日のガチャによる結果表示)
-#     ogstr2 = Column(String, default="")
-#     ogstr3 = Column(String, default="")
-#     ogstr4 = Column(String, default="")
-#     ogstr5 = Column(String, default="")
-#     ogint1 = Column(Integer, default=0)  # cog.core_gacha.py使用中(１日のガチャによる経験値量の収支)#//userid:101のみ９倍デーのガチャ合計カウント
-#     ogint2 = Column(Integer, default=0)
-#     ogint3 = Column(Integer, default=0)
-#     ogint4 = Column(Integer, default=0)
-#     ogint5 = Column(Integer, default=0)
-#     ogint6 = Column(Integer, default=0)
-#     ogint7 = Column(Integer, default=0)
-#     ogint8 = Column(Integer, default=0)
-#     ogint9 = Column(Integer, default=0)
-#     ogint10 = Column(Integer, default=0)
-#     ogbool1 = Column(Boolean, default=False)
-#     ogbool2 = Column(Boolean, default=False)
-#     ogbool3 = Column(Boolean, default=False)
-#     ogbool4 = Column(Boolean, default=False)
-#     ogbool5 = Column(Boolean, default=False)
-#     ogbool6 = Column(Boolean, default=False)
-#     ogbool7 = Column(Boolean, default=False)
-#     ogbool8 = Column(Boolean, default=False)
-#     ogbool9 = Column(Boolean, default=False)
-#     ogbool10 = Column(Boolean, default=False)
-# 
-# 
-# Base2.metadata.create_all(engine2)
-# Session2 = sessionmaker(bind=engine2)
-# session2 = Session2()
 
 
 class Sqltest1(commands.Cog):
