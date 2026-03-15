0f8fbf423a71264bb9615c3de2fe0d31292190b7
syunngiku-miniPC <syunngikusub3@outlook.jp>
2026-03-15 14:46:31 +0900

    修正版

--- cogs/cpresent.py
+++ cogs/cpresent.py
@@ -1,4 +1,4 @@
-from discord.ext import commands, tasks
+from discord.ext import commands
 from datetime import datetime, timedelta
 from discord import ButtonStyle, app_commands
 from database import User, session
@@ -12,12 +12,12 @@ class Lottery(discord.ui.View):  # 抽選コマンド
         super().__init__(timeout=None)
         self.bot = bot
 
-    @discord.ui.button(label="参加", style=ButtonStyle.green, emoji="✅", custom_id="present")
+    @discord.ui.button(label="参加", style=ButtonStyle.green, emoji="✅", custom_id="present-a")
     async def pressedLotteryButton(self, interaction: discord.Interaction, button: discord.ui.button):
         await interaction.response.defer(thinking=True, ephemeral=True)
         oubouser = session.query(User).filter_by(userid=interaction.user.id).first()
 
-        if oubouser is None:
+        if not oubouser:
             userdb = User(userid=interaction.user.id, username=interaction.user.name, displayname=interaction.user.display_name)
             session.add(userdb)
             session.commit()
@@ -27,16 +27,17 @@ class Lottery(discord.ui.View):  # 抽選コマンド
             await interaction.followup.send("すでに参加済みです。当選発表までお待ちください。", ephemeral=True)
             return
 
-    @discord.ui.button(label="参加状況確認", style=ButtonStyle.blurple, custom_id="check")
+    @discord.ui.button(label="参加状況確認", style=ButtonStyle.blurple, custom_id="check-b")
     async def pressedCheckButton(self, interaction: discord.Interaction, button: discord.ui.button):
+        await interaction.response.defer(thinking=True, ephemeral=True)
         oubouser = session.query(User).filter_by(userid=interaction.user.id).first()
-        if oubouser is None:
-            await interaction.response.send_message("まだ参加されていません。参加ボタンを押して参加してください。", ephemeral=True)
+        if not oubouser:
+            await interaction.followup.send("まだ参加されていません。参加ボタンを押して参加してください。", ephemeral=True)
             return
         else:
-            await interaction.response.send_message("参加完了してます。当選発表までお待ちください。", ephemeral=True)
+            await interaction.followup.send("参加完了してます。当選発表までお待ちください。", ephemeral=True)
 
-    @discord.ui.button(label="企画終了", style=ButtonStyle.red, custom_id="delevent")
+    @discord.ui.button(label="企画終了", style=ButtonStyle.red, custom_id="delevent-c")
     async def pressedDeleventButton(self, interaction: discord.Interaction, button: discord.ui.button):
         if interaction.user.id == config.syunngiku_id:
             await interaction.message.delete()
@@ -47,7 +48,6 @@ class Lottery(discord.ui.View):  # 抽選コマンド
 class Present(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        self.send_task.start()
 
 # !--------------------------------------------------------------------
 
@@ -81,28 +81,33 @@ class Present(commands.Cog):
             timestamp=datetime.now()
         )
         await interaction.response.send_message("送信しました", ephemeral=True)
-        await interaction.channel.send(embed=present_embed)
+        await interaction.channel.send("突如始まる運のみで決まるプレゼント企画(笑)", embed=present_embed)
         await interaction.channel.send(view=Lottery(self.bot))
 
-    @tasks.loop(seconds=60)
-    async def send_task(self):
-        now = datetime.now()
-        if now.hour == 00 and now.minute == 00:
-            ch = await self.bot.fetch_channel(config.channel.nitice)
-            oubouser = session.query(User).all()
-            if len(oubouser) == 0:
-                await ch.send("応募者はいませんでした。")
-                return
-            winner_num = random.randint(1, len(oubouser))
-            for user in oubouser:
-                if user.no == winner_num:
-                    embed = discord.Embed(
-                        title="当選発表",
-                        description=f"# 当選者発表\n## <@{user.userid}>さん\nおめでとうございます!!!\n景品は星街すいせいバンドルか1000V-BUCKSのどちらかになります。\nご希望の景品をDMにてお知らせください。",
-                        color=0x87ea00,
-                        timestamp=datetime.now()
-                    )
-                    await ch.send(embed=embed)
+    @app_commands.command(name="happyou", description="buttonのみ送信")
+    @app_commands.checks.has_role(config.role.administrater_role_id)
+    @app_commands.guild_only()
+    async def tousennsya(self, interaction: discord.Interaction):
+        oubouser = session.query(User).all()
+        if len(oubouser) == 0:
+            await interaction.channel.send("応募者はいませんでした。")
+            return
+        winner_num = random.randint(1, len(oubouser))
+        for user in oubouser:
+            if user.no == winner_num:
+                embed = discord.Embed(
+                    title="当選発表",
+                    description=f"# 当選者発表\n## <@{user.userid}>さん\nおめでとうございます!!!\n景品は星街すいせいバンドルか1000V-BUCKSのどちらかになります。\nご希望の景品をDMにてお知らせください。",
+                    color=0x87ea00,
+                    timestamp=datetime.now()
+                )
+                await interaction.channel.send(f"<@{user.userid}>", embed=embed)
+    
+    @app_commands.command(name="button", description="buttonのみ送信")
+    @app_commands.checks.has_role(config.role.administrater_role_id)
+    @app_commands.guild_only()
+    async def send_button(self, interaction: discord.Interaction):
+        await interaction.channel.send(view=Lottery(self.bot))
 
 
 async def setup(bot: commands.Bot):
